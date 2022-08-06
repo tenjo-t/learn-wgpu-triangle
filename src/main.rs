1318c1bf671a056adb420f17b38cf69a134a2469
tenjo-t <tenjo0314takashi@gmail.com>
2022-08-06 13:18:51 +0900

    レンダリングを記述する

--- src/main.rs
+++ src/main.rs
@@ -152,6 +152,49 @@ fn main() {
         multiview: None,
     });
 
+    let render = |device: &wgpu::Device,
+                  queue: &wgpu::Queue,
+                  surface: &wgpu::Surface,
+                  render_pipeline: &wgpu::RenderPipeline,
+                  vertex_buffer: &wgpu::Buffer| {
+        let output = surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&render_pipeline);
+            render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+            render_pass.draw(0..3, 0..1)
+        }
+
+        queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    };
+
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             window_id,
@@ -175,6 +218,18 @@ fn main() {
             }
             _ => {}
         },
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            match render(&device, &queue, &surface, &render_pipeline, &vertex_buffer) {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => {
+                    resize(&device, &surface, &mut config, size);
+                }
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
         _ => {}
     });
 }
